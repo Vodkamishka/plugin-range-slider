332b8b3e71c063960da3859ddd7fb7cc31dd1747
Deynega Andrey <andrtomsk@narod.ru>
2019-11-05 13:03:17 +0700

    create classes View, Model, Controller and instances this classes

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+(function () {
+    var View = /** @class */ (function () {
+        function View() {
+        }
+        return View;
+    }());
+    var Model = /** @class */ (function () {
+        function Model() {
+        }
+        return Model;
+    }());
+    var Controller = /** @class */ (function () {
+        function Controller() {
+        }
+        return Controller;
+    }());
+    window.onload = function () {
+        var view = new View();
+        var model = new Model();
+        var controller = new Controller();
+        console.log('work');
+    };
+})();
---
+++ script.ts
@@ -0,0 +1,22 @@
+(() => {
+
+class View {
+
+}
+
+class Model {
+
+}
+
+class Controller {
+
+}
+
+window.onload = function () {
+    const view = new View()
+    const model = new Model()
+    const controller = new Controller()
+    console.log('work')
+}
+
+})()
