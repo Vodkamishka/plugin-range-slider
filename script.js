af40315afbd3ba3354daf33fe9d0cb03eeabbf5a
Deynega Andrey <andrtomsk@narod.ru>
2019-11-10 11:30:09 +0700

    used funtion createElement for building tags of plugin

--- script.js
+++ script.js
@@ -1,8 +1,71 @@
-"use strict";
 (function () {
-    var View = /** @class */ (function () {
-        function View() {
+    function createElement(tag, props) {
+        var children = [];
+        for (var _i = 2; _i < arguments.length; _i++) {
+            children[_i - 2] = arguments[_i];
         }
+        var element;
+        element = document.createElement(tag);
+        element.classList.add("" + props["class"]);
+        element.type = props.type;
+        element.setAttribute('value', props.value);
+        children.forEach(function (el) {
+            if (typeof el === 'string') {
+                el = document.createTextNode(el);
+            }
+            element.appendChild(el);
+        });
+        return element;
+    }
+    var View = /** @class */ (function () {
+        function View(random) {
+            this.wrapper = document.getElementById(random.toString());
+            this.create();
+            if (this.wrapper !== null) {
+                this.between = this.wrapper.querySelector('.between');
+                this.begin = this.wrapper.querySelector('.begin');
+                this.end = this.wrapper.querySelector('.end');
+            }
+            console.log(11111);
+        }
+        View.prototype.create = function () {
+            if (this.wrapper !== null) {
+                var num1 = createElement('div', { "class": 'num1' });
+                var num2 = createElement('div', { "class": 'num2' });
+                var between = createElement('div', { "class": 'between' });
+                var begin = createElement('div', { "class": 'begin' });
+                var end = createElement('div', { "class": 'end' });
+                var slider1 = createElement('input', { "class": 'slider1', type: 'range', min: '0', max: '25000', value: '5000' });
+                var slider2 = createElement('input', { "class": 'slider2', type: 'range', min: '0', max: '25000', value: '15000' });
+                var range = createElement('div', { "class": 'range' }, slider1, slider2, between, begin, end, num1, num2);
+                var p = createElement('p', { "class": 'p' }, "Панель конфигурации:");
+                var min = createElement('input', { "class": 'min', type: 'text', value: '0' });
+                var labelMin = createElement('label', { "class": 'label' }, 'Мин. диапазона', min);
+                var max = createElement('input', { "class": 'max', type: 'text', value: '25000' });
+                var labelMax = createElement('label', { "class": 'label' }, 'Макс. диапазона', max);
+                var value1 = createElement('input', { "class": 'value1', type: 'text', value: '5000' });
+                var labelVal1 = createElement('label', { "class": 'labelVal1' }, 'Значение 1', value1);
+                var value2 = createElement('input', { "class": 'value2', type: 'text', value: '15000' });
+                var labelVal2 = createElement('label', { "class": 'labelVal2' }, 'Значение 2', value2);
+                var flag1 = createElement('input', { "class": 'flag1', type: 'checkbox', value: '' });
+                var labelFlag1 = createElement('label', { "class": 'labelFlag1' }, 'Откл. бегунок 1', flag1);
+                var flag2 = createElement('input', { "class": 'flag2', type: 'checkbox', value: '' });
+                var labelFlag2 = createElement('label', { "class": 'labelFlag2' }, 'Откл. бегунок 2', flag2);
+                var inpNum1 = createElement('input', { "class": 'inpNum1', type: 'checkbox', value: '' });
+                var labelNum1 = createElement('label', { "class": 'labelNum1' }, 'Откл. значение 1', inpNum1);
+                var inpNum2 = createElement('input', { "class": 'inpNum2', type: 'checkbox', value: '' });
+                var labelNum2 = createElement('label', { "class": 'labelNum2' }, 'Откл. значение 2', inpNum2);
+                var rotate = createElement('input', { "class": 'rotateSlider', type: 'checkbox', value: '' });
+                var labelRotate = createElement('label', { "class": 'labelRotate' }, 'Вкл. вертикальный вид', rotate);
+                var step = createElement('input', { "class": 'step', type: 'text', value: '' });
+                var labelStep = createElement('label', { "class": 'label' }, 'Размер шага 1', step);
+                var step2 = createElement('input', { "class": 'step2', type: 'text', value: '' });
+                var labelStep2 = createElement('label', { "class": 'label' }, 'Размер шага 2', step2);
+                var panel = createElement('div', { "class": 'panel' }, p, labelMin, labelMax, labelVal1, labelVal2, labelFlag1, labelFlag2, labelNum1, labelNum2, labelRotate, labelStep, labelStep2);
+                this.wrapper.appendChild(panel);
+                this.wrapper.appendChild(range);
+            }
+        };
         return View;
     }());
     var Model = /** @class */ (function () {
@@ -15,10 +78,22 @@
         }
         return Controller;
     }());
+    /* module.exports = {
+        View,
+        Model,
+        Controller,
+        createElement
+    }*/
     window.onload = function () {
-        var view = new View();
-        var model = new Model();
-        var controller = new Controller();
-        console.log('work');
+        var wrapper = document.querySelectorAll('.wrapper');
+        if (wrapper !== null) {
+            wrapper.forEach(function (el) {
+                var random = Math.floor(Math.random() * 10000000000000);
+                el.id = random.toString();
+                var view = new View(random);
+                var model = new Model();
+                var controller = new Controller();
+            });
+        }
     };
 })();
--- script.ts
+++ script.ts
@@ -1,8 +1,99 @@
+
+
+
 (() => {
 
-class View {
+    interface Props {
+        class?: string
+        type?: string
+        value?: string
+        min?: string
+        max?: string
+    }
+        
+    function createElement(tag: string, props: Props, ...children: any) {
+        let element: any
+        element = document.createElement(tag)
+        element.classList.add(`${props.class}`)
+        element.type = props.type
+        element.setAttribute('value', props.value)
+       
+        children.forEach((el: any) => {
+            if (typeof el === 'string') {el = document.createTextNode(el)}
+            element.appendChild(el)
+        }) 
+        return element
+    }
 
-}
+    class View {
+        wrapper: HTMLElement | null
+        between!: HTMLElement | null
+        begin!: HTMLElement | null
+        end!: HTMLElement | null
+       
+        constructor(random: number) { 
+           this.wrapper = document.getElementById(random.toString()) 
+           this.create()
+           if ( this.wrapper !== null) {
+                this.between = this.wrapper.querySelector('.between')
+                this.begin = this.wrapper.querySelector('.begin')
+                this.end = this.wrapper.querySelector('.end')
+            }
+            console.log(11111)
+         } 
+        create () {
+            if (this.wrapper !== null){
+                let num1:HTMLElement = createElement('div', {class: 'num1'})
+                let num2:HTMLElement = createElement('div', {class: 'num2'})
+                let between:HTMLElement = createElement('div', {class: 'between'})
+                let begin:HTMLElement = createElement('div', {class: 'begin'})
+                let end:HTMLElement = createElement('div', {class: 'end'})
+                
+                let slider1:HTMLInputElement = createElement('input', {class: 'slider1', type: 'range', min: '0', max: '25000', value: '5000'})
+                let slider2:HTMLInputElement = createElement('input', {class: 'slider2', type: 'range', min: '0', max: '25000', value: '15000'})
+    
+                let range:HTMLElement = createElement('div', {class: 'range'}, slider1, slider2, between, begin, end, num1, num2)
+    
+                let p:HTMLElement = createElement('p', {class: 'p'}, "Панель конфигурации:")
+    
+                let min:HTMLInputElement = createElement('input', {class: 'min', type: 'text', value: '0'})
+                let labelMin:HTMLElement = createElement('label', {class: 'label'}, 'Мин. диапазона', min)
+                let max:HTMLInputElement = createElement('input', {class: 'max', type: 'text', value: '25000'})
+                let labelMax:HTMLElement = createElement('label', {class: 'label'}, 'Макс. диапазона', max)
+                let value1:HTMLInputElement = createElement('input', {class: 'value1', type: 'text', value: '5000'})
+                let labelVal1:HTMLElement = createElement('label', {class: 'labelVal1'}, 'Значение 1', value1)
+                let value2:HTMLInputElement = createElement('input', {class: 'value2', type: 'text', value: '15000'})
+                let labelVal2:HTMLElement = createElement('label', {class: 'labelVal2'}, 'Значение 2', value2)
+                let flag1:HTMLInputElement = createElement('input', {class: 'flag1', type: 'checkbox', value: ''})
+                let labelFlag1:HTMLElement = createElement('label', {class: 'labelFlag1'}, 'Откл. бегунок 1', flag1)
+                let flag2:HTMLInputElement = createElement('input', {class: 'flag2', type: 'checkbox', value: ''})
+                let labelFlag2:HTMLElement = createElement('label', {class: 'labelFlag2'}, 'Откл. бегунок 2', flag2)
+                let inpNum1:HTMLInputElement = createElement('input', {class: 'inpNum1', type: 'checkbox', value: ''})
+                let labelNum1:HTMLElement = createElement('label', {class: 'labelNum1'}, 'Откл. значение 1', inpNum1)
+                let inpNum2:HTMLInputElement = createElement('input', {class: 'inpNum2', type: 'checkbox', value: ''})
+                let labelNum2:HTMLElement = createElement('label', {class: 'labelNum2'}, 'Откл. значение 2', inpNum2)
+                let rotate:HTMLInputElement = createElement('input', {class: 'rotateSlider', type: 'checkbox', value: ''})
+                let labelRotate:HTMLElement = createElement('label', {class: 'labelRotate'}, 'Вкл. вертикальный вид', rotate)
+                let step:HTMLInputElement = createElement('input', {class: 'step', type: 'text', value: ''})
+                let labelStep:HTMLElement = createElement('label', {class: 'label'}, 'Размер шага 1', step)
+                let step2:HTMLInputElement = createElement('input', {class: 'step2', type: 'text', value: ''})
+                let labelStep2:HTMLElement = createElement('label', {class: 'label'}, 'Размер шага 2', step2)
+                
+                
+                let panel:HTMLElement = createElement('div', {class: 'panel'}, p, labelMin, labelMax, labelVal1, 
+                labelVal2, labelFlag1, labelFlag2, labelNum1, labelNum2, labelRotate, labelStep, labelStep2)
+    
+                this.wrapper.appendChild(panel)
+                this.wrapper.appendChild(range)
+            }
+        }
+
+       
+        
+    }
+    
+    
+ 
 
 class Model {
 
@@ -12,11 +103,29 @@ class Controller {
 
 }
 
+/* module.exports = {
+    View, 
+    Model, 
+    Controller, 
+    createElement  
+}*/
+
+
 window.onload = function () {
-    const view = new View()
-    const model = new Model()
-    const controller = new Controller()
-    console.log('work')
-}
+
+    let wrapper:NodeListOf<HTMLElement> | null  = document.querySelectorAll('.wrapper')
+    
+    if (wrapper !== null){wrapper.forEach((el:HTMLElement) => {
+        let random: number = Math.floor(Math.random()*10000000000000)
+        el.id = random.toString()
+        const view = new View(random)
+        const model = new Model()
+        const controller = new Controller()
+    })}
+    }
+
+
 
 })()
+
+
